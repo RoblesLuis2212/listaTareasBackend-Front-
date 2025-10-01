f2e84dfe31f9e065172ac9f94d68f9a4846cb454
Luis Robles <luisgeremiasrobles@gmail.com>
2025-10-01 19:49:34 -0300

    Se agrego un formulario para agregar las tareas y se lo valido

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,21 +1,46 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { useForm } from 'react-hook-form';
 
 
 const FormularioTarea = () => {
+    //validaciones
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        reset
+    } = useForm()
+
+    const postValidaciones = (data) => {
+        data.id = crypto.randomUUID();
+        console.log(data);
+        reset();
+    }
+
     return (
         <section className='container border border-white'>
-            <Form>
-                <Form.Group className="mb-3">
-                    <Form.Control type="text" placeholder="ingresa una tarea" />
-                    <Form.Text className="text-danger">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
+            <Form onSubmit={handleSubmit(postValidaciones)}>
+                <Form.Group className="d-flex justify-content-betwen py-2">
+                    <Form.Control type="text" placeholder="ingresa una tarea" {...register("tarea", {
+                        required: "Este campo es obligatorio",
+                        minLength: {
+                            value: 10,
+                            message: "la tarea debe contener minimo 10 caracteres"
+                        },
+                        maxLength: {
+                            value: 60,
+                            message: "la tarea debe contener maximo 60 "
+                        }
+                    })} />
+                    <Button variant="primary" type="submit" className='ms-1'>
+                        agregar
+                    </Button>
                 </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
+                <Form.Text className="text-danger">
+                    {errors.tarea?.message}
+                </Form.Text>
             </Form>
         </section>
     );
